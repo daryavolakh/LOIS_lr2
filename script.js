59cc32ffef6a664266e5691dd1c46bc3daac68b2
Darya <dasha27061999@mail.ru>
2019-03-12 13:11:59 +0300

    added some stuff

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 */
 
 var constants = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '0'];
-var operations = ['~', '-', '>', '|', '&', '!'];
+var operations = ['~', '->', '|', '&', '!'];
 var formula;
 
 function findInArray(array, temp) {
@@ -78,8 +78,7 @@ function changeConstant(constant){
     }
 }
 
-function createTable(variables){
-    var table = new Array();
+function createTable(table, variables){
     for (var indexI = variables.length - 1; indexI >= 0; indexI--){
         var row = new Array();
         var changeConst = Math.pow(2, indexI);
@@ -93,14 +92,53 @@ function createTable(variables){
         }
         table.push(row);
     }
-
     console.log(table);
+}
+
+function findOperation(subFromula){
+    for (var index = 0; index < subFormula.length; index++){
+        if (findInArray(operations, formula[index])){
+            return formula[index];
+        }
+
+        if (formula[index] == '-' && formula[index + 1] == ">"){
+            var operation = formula[index] + formula[index + 1];
+            return operation;
+        }
+    }
+}
+
+//нужно как-то доставать нужные связки из больших формул или заменять обработанные формулы какими-то буквами (для упрощения).
+//один массив, где на первом месте подформула, на втором - соответствующая ей колонка со значениями. 
+
+function withOutName(table, subFormulas){  /* rename */
+    for (var index = subFormulas.length - 1; index >= 0; index--){
+        var operation = findOperation(subFormulas[index]);
+        var row = new Array();
+        if (operation == "!"){
+            for (var indexJ = 0; indexJ < Math.pow(2,variables.length); indexJ++){
+                place = ; /*индекс, соответствующий переменной в ТИ*/
+                var value = changeConstant(variable[place]);
+                row.push(value);
+            }
+
+        } else if (operation == "&"){
+
+        } else if (operation == "|"){
+            
+        } else if (operation == "~"){
+            
+        } else if (operation == "->"){
+
+        }
+    }
 }
 
 function start() {
     var input = document.getElementById("form");
     var subFormulas = new Array();
     var variables = new Array();
+    var table = new Array();
     formula = input.elements[0].value;
 
     if (formula == "") {
@@ -109,7 +147,7 @@ function start() {
     }
 
     findSubformulas(subFormulas, variables);
-    createTable(variables);
+    createTable(table, variables);
     var body = document.querySelector("body");
     var newDiv = document.createElement("div");
     var ol = document.createElement("ol");
