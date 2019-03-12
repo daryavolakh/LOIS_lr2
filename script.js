5b9a5b3ddf0cc30f6b2a12b1a4c9cad5c1ac3f52
Darya <dasha27061999@mail.ru>
2019-03-12 12:39:52 +0300

    added part from lab1 and creating table for variables

---
+++ script.js
@@ -0,0 +1,139 @@
+/*
+    //////////////////////////////////////////////////////////////////////////////////////
+// Лабораторная работа 2 по дисциплине ЛОИС
+// Выполнена студенткой группы 621702 БГУИР Волах Д.Ю.
+// **************.
+// 26.02.2019
+//
+// https://learn.javascript.ru/
+//
+    //////////////////////////////////////////////////////////////////////////////////////
+*/
+
+var constants = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '0'];
+var operations = ['~', '-', '>', '|', '&', '!'];
+var formula;
+
+function findInArray(array, temp) {
+    for (var index = 0; index < array.length; index++) {
+        if (array[index].toString() == temp.toString()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getSubFormula(memory, subFormulas, priority) {
+    var open = 0;
+    var close = 0;
+    var subFormula = new Array();
+
+    for (var index = memory; index < formula.length; index++) {
+        subFormula.push(formula[index]);
+        if (formula[index] == '(') {
+            open++;
+        }
+
+        if (formula[index] == ')') {
+            close++;
+        }
+
+        if (open == close) {
+            if (findInArray(subFormulas, subFormula) == false) {
+                var temp = new Array();
+                temp.push(priority, subFormula);
+                subFormulas.push(temp);
+            }
+            return;
+        }
+    }
+}
+
+function findSubformulas(subFormulas, variables) {
+    var numOfOpen = 0;
+    //var numOfClose = 0;
+    for (var index = 0; index < formula.length; index++) {
+        if (formula[index] == '(') {
+            numOfOpen++;
+            getSubFormula(index, subFormulas, numOfOpen);
+        }
+
+        else if (formula[index] == ')') {
+            numOfOpen--;
+        }
+
+        else if (findInArray(constants, formula[index]) && !findInArray(variables, formula[index])) {
+            variables.push(formula[index]);
+            console.log("VARIABLE -> " + formula[index]);
+        }
+    }
+}
+
+function changeConstant(constant){
+    if (constant == 0){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+function createTable(variables){
+    var table = new Array();
+    for (var indexI = variables.length - 1; indexI >= 0; indexI--){
+        var row = new Array();
+        var changeConst = Math.pow(2, indexI);
+        var constant = 0;
+        for (var indexJ = 0; indexJ < Math.pow(2,variables.length); indexJ++){
+            row.push(constant);
+            console.log("CHANGE CONSTANT: " + changeConst);
+            if((indexJ + 1) % changeConst == 0){
+                constant = changeConstant(constant);
+            }
+        }
+        table.push(row);
+    }
+
+    console.log(table);
+}
+
+function start() {
+    var input = document.getElementById("form");
+    var subFormulas = new Array();
+    var variables = new Array();
+    formula = input.elements[0].value;
+
+    if (formula == "") {
+        alert("Empty field! Please, enter formula!")
+        return;
+    }
+
+    findSubformulas(subFormulas, variables);
+    createTable(variables);
+    var body = document.querySelector("body");
+    var newDiv = document.createElement("div");
+    var ol = document.createElement("ol");
+    var mainDiv = document.getElementById("gener_div");
+    mainDiv.innerHTML = "";
+
+    newDiv.setAttribute("class", "div_with_ol");
+
+    for (index1 = 0; index1 < subFormulas.length; index1++) {
+        var str = '';
+        for (index2 = 0; index2 < subFormulas[index1].length; index2++) {
+            str += subFormulas[index1][index2];
+        }
+        var content = document.createTextNode(str);
+        var li = document.createElement("li");
+        li.appendChild(content);
+        ol.appendChild(li);
+    }
+    newDiv.appendChild(ol);
+    mainDiv.appendChild(newDiv);
+
+    var answer = document.createElement("p");
+    var answerContent = document.createTextNode("Answer: " + subFormulas.length + ".");
+    answer.appendChild(answerContent);
+    mainDiv.appendChild(answer);
+}
+//((P~Q)~((!W)&(!P)))
