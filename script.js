cd5a6699b9dde9090e5d1c46c2b03039c869d754
Darya <dasha27061999@mail.ru>
2019-03-26 23:05:14 +0300

    Almost done

--- script.js
+++ script.js
@@ -10,9 +10,13 @@
     //////////////////////////////////////////////////////////////////////////////////////
 */
 
-var constants = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '0'];
+var symbols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '0'];
 var operations = ['~', '->', '|', '&', '!'];
+var constants = ['1', '0'];
 var formula;
+var table;
+var variables;
+var subFormulas;
 
 function findInArray(array, temp) {
     for (var index = 0; index < array.length; index++) {
@@ -26,10 +30,10 @@ function findInArray(array, temp) {
 function getSubFormula(memory, subFormulas, priority) {
     var open = 0;
     var close = 0;
-    var subFormula = new Array();
+    var subFormula = '';
 
     for (var index = memory; index < formula.length; index++) {
-        subFormula.push(formula[index]);
+        subFormula += formula[index];
         if (formula[index] == '(') {
             open++;
         }
@@ -41,7 +45,8 @@ function getSubFormula(memory, subFormulas, priority) {
         if (open == close) {
             if (findInArray(subFormulas, subFormula) == false) {
                 var temp = new Array();
-                temp.push(priority, subFormula);
+                temp.push(priority);
+                temp.push(subFormula);
                 subFormulas.push(temp);
             }
             return;
@@ -51,7 +56,6 @@ function getSubFormula(memory, subFormulas, priority) {
 
 function findSubformulas(subFormulas, variables) {
     var numOfOpen = 0;
-    //var numOfClose = 0;
     for (var index = 0; index < formula.length; index++) {
         if (formula[index] == '(') {
             numOfOpen++;
@@ -62,15 +66,14 @@ function findSubformulas(subFormulas, variables) {
             numOfOpen--;
         }
 
-        else if (findInArray(constants, formula[index]) && !findInArray(variables, formula[index])) {
+        else if ((findInArray(symbols, formula[index]) || findInArray(constants, formula[index])) && !findInArray(variables, formula[index])) {
             variables.push(formula[index]);
-            console.log("VARIABLE -> " + formula[index]);
         }
     }
 }
 
-function changeConstant(constant){
-    if (constant == 0){
+function changeConstant(constant) {
+    if (constant == 0) {
         return 1;
     }
     else {
@@ -78,67 +81,210 @@ function changeConstant(constant){
     }
 }
 
-function createTable(table, variables){
-    for (var indexI = variables.length - 1; indexI >= 0; indexI--){
+function calculateNumberOfVars(variables){
+    var number = 0;
+    for (var indexI = 0; indexI < variables.length; indexI++) {
+        if (!findInArray(constants, variables[indexI])){
+            number++;
+        }
+    }
+    return number;
+}
+
+function createTable(table, variables) {
+    var number = calculateNumberOfVars(variables) - 1;
+    var numberOfValues = number + 1;
+    for (var indexI = variables.length - 1; indexI >= 0; indexI--) {
         var row = new Array();
-        var changeConst = Math.pow(2, indexI);
-        var constant = 0;
-        for (var indexJ = 0; indexJ < Math.pow(2,variables.length); indexJ++){
-            row.push(constant);
-            console.log("CHANGE CONSTANT: " + changeConst);
-            if((indexJ + 1) % changeConst == 0){
-                constant = changeConstant(constant);
+        if (!findInArray(constants, variables[indexI])) {
+            var changeConst = Math.pow(2, number);    //чередование 0 и 1 в ТИ 
+            var constant = 0;
+            for (var indexJ = 0; indexJ < Math.pow(2, numberOfValues); indexJ++) {
+                row.push(constant);
+                if ((indexJ + 1) % changeConst == 0) {
+                    constant = changeConstant(constant);
+                }
+            }
+            number--;
+        } else {
+            var constant;
+            if (variables[indexI] == '1') {
+                constant = 1;
+            } else if(variables[indexI] == '0'){
+                constant = 0;
+            }
+
+            for (var indexJ = 0; indexJ < Math.pow(2, numberOfValues); indexJ++) {
+                row.push(constant);
             }
         }
-        table.push(row);
-    }
-    console.log(table);
-}
 
-function findOperation(subFromula){
-    for (var index = 0; index < subFormula.length; index++){
-        if (findInArray(operations, formula[index])){
-            return formula[index];
-        }
-
-        if (formula[index] == '-' && formula[index + 1] == ">"){
-            var operation = formula[index] + formula[index + 1];
-            return operation;
-        }
+        var field = new Array();
+        field.push(variables[indexI]);
+        field.push(row);
+        table.push(field);
     }
 }
 
-//нужно как-то доставать нужные связки из больших формул или заменять обработанные формулы какими-то буквами (для упрощения).
-//один массив, где на первом месте подформула, на втором - соответствующая ей колонка со значениями. 
+function findIndexOfOperation(sub) {
+    var open = 0;
+    var close = 0;
+    for (var index = 0; index < sub.length; index++) {
+        if (open - close == 1) {
+            if (findInArray(operations, sub[index])) {
+                return index;
+            }
 
-function withOutName(table, subFormulas){  /* rename */
-    for (var index = subFormulas.length - 1; index >= 0; index--){
-        var operation = findOperation(subFormulas[index]);
+            else if (sub[index] == '-' && sub[index + 1] == ">") {
+                return index;
+            }
+        }
+
+        if (sub[index] == "(") {
+            open++;
+        }
+
+        if (sub[index] == ")") {
+            close++;
+        }
+    }
+}
+
+function findCol(variable) {
+    for (var index = 0; index < table.length; index++) {
+        if (table[index][0] == variable) {
+            return table[index][1];
+        }
+
+    }
+}
+
+//1, 0~A, (0~A)&1, (((((A&B)&C)&D)&E)&F)...&K
+
+function findLeftPart(sub, indexOfOperation) {
+    var leftPart = "";
+    for (var index = 1; index < indexOfOperation; index++) {
+        leftPart += sub[index];
+    }
+    return leftPart;
+}
+
+function findRightPart(sub, indexOfOperation) {
+    var begin = indexOfOperation + 1;
+    var rightPart = "";
+    if (sub[indexOfOperation] == "-") {
+        begin++;
+    }
+
+    for (var index = begin; index < sub.length - 1; index++) {
+        rightPart += sub[index];
+    }
+
+    return rightPart;
+}
+
+function mainCalculations(table, subFormulas) {
+    for (var index = subFormulas.length - 1; index >= 0; index--) {
+        var indexOfOperation = findIndexOfOperation(subFormulas[index][1]);
+        var operation = subFormulas[index][1][indexOfOperation];
+
+        if (operation == "-" && subFormulas[index][1][indexOfOperation + 1] == ">") {
+            operation += ">";
+        }
         var row = new Array();
-        if (operation == "!"){
-            for (var indexJ = 0; indexJ < Math.pow(2,variables.length); indexJ++){
-                place = ; /*индекс, соответствующий переменной в ТИ*/
-                var value = changeConstant(variable[place]);
+        if (operation == "!") {
+            var variable = findRightPart(subFormulas[index][1], indexOfOperation);
+            var col = findCol(variable);
+            for (var indexJ = 0; indexJ < col.length; indexJ++) {
+                var value = changeConstant(col[indexJ]);
                 row.push(value);
             }
+            var field = new Array();
+            field.push(subFormulas[index][1]);
+            field.push(row);
+            table.push(field);
+        } else {
+            var leftPart = findLeftPart(subFormulas[index][1], indexOfOperation);
+            var rightPart = findRightPart(subFormulas[index][1], indexOfOperation);
+            var colForLeftPart = findCol(leftPart);
+            var colForRightPart = findCol(rightPart);
 
-        } else if (operation == "&"){
-
-        } else if (operation == "|"){
-            
-        } else if (operation == "~"){
-            
-        } else if (operation == "->"){
-
+            if (operation == "&") {
+                var newCol = new Array();
+                for (var indexI = 0; indexI < colForLeftPart.length; indexI++) {
+                    if (colForLeftPart[indexI] == 1 && colForRightPart[indexI] == 1) {
+                        newCol.push(1);
+                    }
+                    else {
+                        newCol.push(0);
+                    }
+                }
+                var newField = new Array();
+                newField.push(subFormulas[index][1]);
+                newField.push(newCol);
+                table.push(newField);
+            } else if (operation == "|") {
+                var newCol = new Array();
+                for (var indexI = 0; indexI < colForLeftPart.length; indexI++) {
+                    if (colForLeftPart[indexI] == 0 && colForRightPart[indexI] == 0) {
+                        newCol.push(0);
+                    }
+                    else {
+                        newCol.push(1);
+                    }
+                }
+                var newField = new Array();
+                newField.push(subFormulas[index][1]);
+                newField.push(newCol);
+                table.push(newField);
+            } else if (operation == "~") {
+                var newCol = new Array();
+                for (var indexI = 0; indexI < colForLeftPart.length; indexI++) {
+                    if ((colForLeftPart[indexI] == 0 && colForRightPart[indexI] == 0) || (colForLeftPart[indexI] == 1 && colForRightPart[indexI] == 1)) {
+                        newCol.push(1);
+                    }
+                    else {
+                        newCol.push(0);
+                    }
+                }
+                var newField = new Array();
+                newField.push(subFormulas[index][1]);
+                newField.push(newCol);
+                table.push(newField);
+            } else if (operation == "->") {
+                var newCol = new Array();
+                for (var indexI = 0; indexI < colForLeftPart.length; indexI++) {
+                    if ((colForLeftPart[indexI] == 0 && colForRightPart[indexI] == 0) || (colForLeftPart[indexI] == 0 && colForRightPart[indexI] == 1) || (colForLeftPart[indexI] == 1 && colForRightPart[indexI] == 1)) {
+                        newCol.push(1);
+                    }
+                    else {
+                        newCol.push(0);
+                    }
+                }
+                var newField = new Array();
+                newField.push(subFormulas[index][1]);
+                newField.push(newCol);
+                table.push(newField);
+            }
         }
     }
+}
+
+function isTautology(){
+    var lastCol = table.length - 1;
+    for (var index = 0; index < table[lastCol][1].length; index++){
+        if (table[lastCol][1][index] == 0) {
+            return false;
+        }
+    } 
+    return true;
 }
 
 function start() {
     var input = document.getElementById("form");
-    var subFormulas = new Array();
-    var variables = new Array();
-    var table = new Array();
+    subFormulas = new Array();
+    variables = new Array();
+    table = new Array();
     formula = input.elements[0].value;
 
     if (formula == "") {
@@ -148,30 +294,15 @@ function start() {
 
     findSubformulas(subFormulas, variables);
     createTable(table, variables);
-    var body = document.querySelector("body");
-    var newDiv = document.createElement("div");
-    var ol = document.createElement("ol");
-    var mainDiv = document.getElementById("gener_div");
-    mainDiv.innerHTML = "";
+    mainCalculations(table, subFormulas);
 
-    newDiv.setAttribute("class", "div_with_ol");
+    console.log("RESULT");
+    console.log(table);
 
-    for (index1 = 0; index1 < subFormulas.length; index1++) {
-        var str = '';
-        for (index2 = 0; index2 < subFormulas[index1].length; index2++) {
-            str += subFormulas[index1][index2];
-        }
-        var content = document.createTextNode(str);
-        var li = document.createElement("li");
-        li.appendChild(content);
-        ol.appendChild(li);
+    if(isTautology()){        
+        alert("Является общезначимой(тавтологией) формулой.")
+    } else {        
+        alert("Не является общезначимой(тавтологией) формулой.")
     }
-    newDiv.appendChild(ol);
-    mainDiv.appendChild(newDiv);
-
-    var answer = document.createElement("p");
-    var answerContent = document.createTextNode("Answer: " + subFormulas.length + ".");
-    answer.appendChild(answerContent);
-    mainDiv.appendChild(answer);
 }
 //((P~Q)~((!W)&(!P)))
